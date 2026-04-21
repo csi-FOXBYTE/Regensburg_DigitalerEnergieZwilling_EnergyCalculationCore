dfc79acb7f536dd51d3f37907db1d94deeb63e7f
Nico Vogel <Nico.Vogel@csi-online.de>
2026-04-21 08:55:22 +0000

    feat: calculator feeds back resolved inputs via return value

--- src/calculate.ts
+++ src/calculate.ts
@@ -8,6 +8,7 @@ export type CalculationResult = {
   energyEfficiencyClass: EnergyEfficiencyClass;
   yearlyCost: number;
   co2Emissions: number;
+  resolvedInput: DETInput;
 };
 
 export type CalculateOptions = {
@@ -17,11 +18,73 @@ export type CalculateOptions = {
 export function calculate(config: DETConfig, input: DETInput, options?: CalculateOptions): CalculationResult {
   const ctx = DETEnergyCaluclator({ config, input });
 
-  const result = {
+  const result: CalculationResult = {
     energyConsumptionPerSquareMeter: ctx.get("totalEnergyDemandPerSquareMeter"),
     energyEfficiencyClass: ctx.get("energyEfficiencyClass"),
     yearlyCost: ctx.get("energyCarrierCost"),
     co2Emissions: ctx.get("co2Emissions"),
+    resolvedInput: {
+      general: {
+        buildingYear: ctx.get("buildingYear"),
+        numberOfStories: ctx.get("numberOfStories"),
+        buildingHeight: ctx.get("buildingHeight"),
+        buildingBaseArea: ctx.get("buildingBaseArea"),
+        livingArea: ctx.get("livingArea"),
+        type: ctx.get("buildingType"),
+      },
+      heat: {
+        heatingSystemConstructionYear: ctx.get("heatingSystemConstructionYear"),
+        primaryEnergyCarrier: ctx.get("primaryEnergyCarrier"),
+        heatingSystemType: ctx.get("heatingSystemType"),
+        heatingSurfaceType: ctx.get("heatingSurfaceType"),
+      },
+      roof: {
+        area: ctx.get("roofArea"),
+        year: ctx.get("roofYear"),
+        hasInsulation: ctx.get("roofHasInsulation"),
+        insulationType: ctx.get("roofInsulationType"),
+        constructionType: ctx.get("roofConstructionType"),
+        insulationThickness: ctx.get("roofInsulationThickness"),
+      },
+      roofWindows: {
+        area: ctx.get("roofWindowsArea"),
+        year: ctx.get("roofWindowsYear"),
+        windowType: ctx.get("roofWindowsType"),
+        uValue: ctx.get("roofWindowsUValue"),
+      },
+      exteriorWallWindows: {
+        area: ctx.get("exteriorWallWindowsArea"),
+        year: ctx.get("exteriorWallWindowsYear"),
+        windowType: ctx.get("exteriorWallWindowsType"),
+        uValue: ctx.get("exteriorWallWindowsUValue"),
+      },
+      topFloor: {
+        area: ctx.get("topFloorArea"),
+        year: ctx.get("topFloorYear"),
+        isAtticHeated: ctx.get("isAtticHeated"),
+        topFloorType: ctx.get("topFloorType"),
+        hasInsulation: ctx.get("topFloorHasInsulation"),
+        insulationThickness: ctx.get("topFloorInsulationThickness"),
+        hasAttic: ctx.get("hasAttic"),
+      },
+      outerWall: {
+        area: ctx.get("outerWallArea"),
+        year: ctx.get("outerWallYear"),
+        hasInsulation: ctx.get("outerWallHasInsulation"),
+        constructionType: ctx.get("outerWallConstructionType"),
+        insulationThickness: ctx.get("outerWallInsulationThickness"),
+      },
+      bottomFloor: {
+        area: ctx.get("bottomFloorArea"),
+        year: ctx.get("bottomFloorYear"),
+        isHeated: ctx.get("bottomFloorIsHeated"),
+        hasInsulation: ctx.get("bottomFloorHasInsulation"),
+        constructionType: ctx.get("bottomFloorConstructionType"),
+        insulationThickness: ctx.get("bottomFloorInsulationThickness"),
+        hasBasement: ctx.get("hasBasement"),
+        isBasementHeated: ctx.get("isBasementHeated"),
+      },
+    },
   };
 
   if (options?.debug) {
