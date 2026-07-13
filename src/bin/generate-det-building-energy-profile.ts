0186871e6143d78d8a9a07e9a0baf318b572139e
Nico Vogel <Nico.Vogel@csi-online.de>
2026-07-13 15:56:50 +0000

    fix: fixed top level await compile error for cjs

--- src/bin/generate-det-building-energy-profile.ts
+++ src/bin/generate-det-building-energy-profile.ts
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
 import { runGenerateDetBuildingEnergyProfileCli } from "../cli/generate-det-building-energy-profile.js";
 
-await runGenerateDetBuildingEnergyProfileCli(process.argv.slice(2), process.cwd());
+void runGenerateDetBuildingEnergyProfileCli(process.argv.slice(2), process.cwd());
